cc448caa01c26d4d94dc5066dd3856a720285c5c
Stephen Mizell <smizell@gmail.com>
2014-03-22 15:38:29 -0500

    Initial commit

---
+++ index.js
@@ -0,0 +1,16 @@
+var uberDoc = require('./exampel.uber'),
+    Halpert = require('halpert'),
+    HyperdescribeUberJson = require('hyperdescribe-uber-json'),
+    HyperdescribeHalJson = require('hyperdescribe-hal-json'),
+
+halpert = new Halpert;
+
+// Register formats to use
+halpert.registerFormat(HyperdescribeUberJson);
+halpert.registerFormat(HyperdescribeHalJson);
+
+// Convert UBER to HAL
+halDoc = halpert.convertFrom(uberDoc, 'application/vnd.amundsen-uber+json').to('application/hal+json')
+
+// Print pretty JSON string to console
+console.log(JSON.stringify(halDoc, null, 2))
